bc84cfe12e46b52f593682ce605acaf12338841d
Davi Campos <davimcampos09@gmail.com>
2023-12-26 14:53:12 -0300

    .

--- src/Dama.java
+++ src/Dama.java
@@ -6,7 +6,9 @@ public class Dama extends Peca{
 
     //metodo para verificar se alguma peca deve virar dama ou nao.
     public void transformaDama(Casa[][] t){
+
         for(int j=0; j<t.length; j++){
+         if(!(t[0][j].getPeca() instanceof Dama) || !(t[7][j].getPeca() instanceof Dama)){      
           if(t[0][j].getPeca().getCorPeca().equalsIgnoreCase("branca")){
               Dama d = new Dama(t[0][j].getPeca().getCorPeca());
               t[0][j] = new Casa(d.getCorPeca(), true, d, 0, j);//se tiver uma peca branca na primeira linha do
@@ -19,6 +21,7 @@ public class Dama extends Peca{
                                                                           //tabuleiro entao ela eh transformada em dama.
               
           }
+         }
         }
     }
     
